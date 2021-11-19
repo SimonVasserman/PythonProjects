1e75a74069db4958cb81eea90ab24abaaafe0627
SimonVasserman <92465650+SimonVasserman@users.noreply.github.com>
2021-11-19 18:58:21 +0200

    homework for lesson12

--- Homework/DZ12_Simon_Wasserman.py
+++ Homework/DZ12_Simon_Wasserman.py
@@ -1,9 +1,17 @@
+from prettytable import PrettyTable
+
+resultTable = PrettyTable()
+
+
 class Homework:
     def __init__(self, name, description, complexity, status):
         self.name = name
         self.description = description
         self.complexity = complexity
         self.status = status
+
+    def __repr__(self):
+        return f"{self.name} | {self.status}"
 
 
 class Student:
@@ -12,67 +20,92 @@ class Student:
         self.age = age
         self.grade = grade
         self.subscribed_courses = []
-        self.homeworks = []
+        self.homeworks = {}
 
-    def add_homework(self, homework):
-        # if homework.status == 1:
-        #     raise Exception
-        self.homeworks.append(homework)
+    def __repr__(self):
+        return f"{self.name} | {self.age} | {self.homeworks}"
 
-    def is_homework_done(self):
+    def add_homework(self, hw_number, homework):
+        self.homeworks[hw_number] = homework
+
+    def is_homework_done(self, hw_number):
         if len(self.homeworks) == 0:
             raise Exception("Homeworks is empty")
-        return self.homeworks[0].status
+        current_hw = self.homeworks.get(hw_number)
+        if current_hw:
+            return current_hw.status
+        return Exception("No such homework")
 
-    def change_status_homework(self):
-        if len(self.homeworks) == 1:
-            return self.homeworks[].status == 1  #?
+    def change_homework_status(self, hw_number, status=True):
+        current_hw = self.homeworks.get(hw_number)
+        if current_hw:
+            current_hw.status = status
+            return current_hw
+        return Exception("No such homework")
 
 
-class Table(Student):
-    pass
+class Table:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
 
     def __str__(self):
-        return f"name: {self.name}, age: {self.age}, grade: {self.grade}"
+        return self.name
+
+    def add_student(self, student_obj):
+        self.students.append(student_obj)
+
+    def get_summary(self):
+        summary = []
+        for student in self.students:
+            summary.append(
+                [
+                    student.name,
+                    student.age,
+                    student.grade,
+                    student.subscribed_courses,
+                    student.homeworks,
+                ]
+            )
+        return summary
+
+    def get_summary_table(self):
+        resultTable.field_names = [
+            "Name",
+            "Age",
+            "Grade",
+            "Subscribed courses",
+            "Homeworks",
+        ]
+        for student in self.students:
+            resultTable.add_row(
+                [
+                    student.name,
+                    student.age,
+                    student.grade,
+                    student.subscribed_courses,
+                    student.homeworks,
+                ]
+            )
+        return resultTable
 
 
-def sort_students_by_age(students):
-    sorted(school, key=lambda student: student.age)
-    return student.age
+school = [Student("Ivan", 25, 0), Student("Peter", 28, 0), Student("Marina", 17, 0), Student("Bogdan", 20, 0)]
 
+h = Homework("OOP intorduction", "Extend logic for Student program", 2, False)
+hw2 = Homework("Python web", "Extend logic for Student program", 2, False)
+hw3 = Homework("OOP Part 3", "Create function to output information about students,status of homeworks", 3, False)
+table = Table(name="Intro Python")
+table_b = Table(name="Python web")
 
-def sort_students_by_grade(students):
-    sorted(school, key=lambda student: student.grade)
-    return student.grade
-
-
-school = [Student("Ivan", 25, 0),
-          Student("Peter", 28, 0),
-          Student("Marina", 20, 0),
-          Student("Georg", 18, 0)]
-
-h = Homework("OOP introduction",
-             "Extend logic for Student program",
-             2, False)
-h1 = Homework("OOP Part 2",
-              "Create class Table for all students",
-              4, False)
-h2 = Homework("OOP Part 3",
-              "Create function to output information about students,status of homeworks",
-              3, False)
-
-# add homework to students:
 for student in school:
-    student.add_homework(h)
-    student.add_homework(h1)
-    student.add_homework(h2)
+    student.add_homework(1, h)
+    student.add_homework(2, hw2)
+    student.add_homework(3, hw3)
+    table.add_student(student)
 
-print(school[0].is_homework_done())
+summary = table.get_summary()
+for item in summary:
+    print(item)
 
-# # sorted by age:
-# for student in school:
-#     print(sort_students_by_age(student))
-#
-# # sorted by grade:
-# for student in school:
-#     print(sort_students_by_grade(student))
+print(table.get_summary_table())
