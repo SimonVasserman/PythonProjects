3311228d966ee977a663b4cdae3754c63e1202dc
SimonVasserman <92465650+SimonVasserman@users.noreply.github.com>
2021-11-23 12:03:04 +0200

    homework for lesson14

--- Homework/Homework14/unit_test_framework/expectations.py
+++ Homework/Homework14/unit_test_framework/expectations.py
@@ -1,7 +1,18 @@
+"""
+This module contains functions of custom  Exception for checking tests of  Expect & Actual
+"""
+
 from unit_test_framework.helper import *
 
 
 def ExpectEqual(actual, expected):
+    """
+    This function tests for strings and name
+    If actual and expected match the test passed
+    if not, there will be an error
+    @param actual:  that will be testing
+    @param expected: expect should be
+    """
     line_number = GetParameter("TestLine")
     test_name = GetParameter("TestName")
 
@@ -15,6 +26,13 @@ def ExpectEqual(actual, expected):
 
 
 def ExpectNotEqual(actual, expected):
+     """
+    This function tests for strings and name
+    If actual and expected  not match the test passed
+    if not, there will be an error
+    @param actual:  that will be testing
+    @param expected: expect should be
+    """
     line_number = GetParameter("TestLine")
     test_name = GetParameter("TestName")
 
@@ -28,20 +46,36 @@ def ExpectNotEqual(actual, expected):
 
 
 def ExpectThrown(block, exception):
-    line_number = GetParameter("TestLine")
-    test_name = GetParameter("TestName")
-    message = GetColor("NAME") + test_name() + "(" + GetColor("LINE") + f"line:{line_number()}" + GetColor(
-        "NAME") + ") - \t"
-    try:
-        block()
-    except type(exception):
-        message += GetColor("SUCCESS") + "SUCCESS"
-    else:
-        message += GetColor("FAILED") + f" exception {type(exception)} don't thrown."
-    print(message)
+    """
+   This function tests for strings and name
+   If block, the test will be passed
+   if not, there will be an error
+    @param block: block the  program
+    @param exception: type exceptions, else typing exception don't thrown.
+   """
+   line_number = GetParameter("TestLine")
+   test_name = GetParameter("TestName")
+   message = GetColor("NAME") + test_name() + "(" + GetColor("LINE") + f"line:{line_number()}" + GetColor(
+       "NAME") + ") - \t"
+   try:
+       block()
+   except type(exception):
+       message += GetColor("SUCCESS") + "SUCCESS"
+   else:
+       message += GetColor("FAILED") + f" exception {type(exception)} don't thrown."
+   print(message)
+
+
 
 
 def ExpectNotThrown(block):
+    """
+       This function tests for strings and name
+       If block, the test will blocked
+       if not, test will be passed
+        @param block: block the  program, typing exception don't thrown.
+        @param exception: type exceptions, else typing success
+       """
     line_number = GetParameter("TestLine")
     test_name = GetParameter("TestName")
     message = GetColor("NAME") + test_name() + "(" + GetColor("LINE") + f"line:{line_number()}" + GetColor(
